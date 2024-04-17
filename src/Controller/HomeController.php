b19e2c259740c83285706fdfc35cfe2bce115c6c
Lao Oceane <oceane.lao1@gmail.com>
2024-04-17 21:18:44 +0200

    style with tailwind homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,39 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
+
     public function index(): Response
     {
+        $reviews = [
+            [
+                'picture' => 'img/john_doe_1.jpg',
+                'name' => 'John Doe',
+                'text' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            ],
+            [
+                'picture' => 'img/jane_doe_1.png',
+                'name' => 'Jane Doe',
+                'text' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            ],
+            [
+                'picture' => 'img/john_doe_2.jpg',
+                'name' => 'John Doe',
+                'text' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            ],
+            [
+                'picture' => 'img/jane_doe_2.jpg',
+                'name' => 'Jane Doe',
+                'text' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            ],
+            [
+                'picture' => 'img/john_doe_3.jpg',
+                'name' => 'John Doe',
+                'text' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            ]
+        ];
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'reviews' => $reviews,
         ]);
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,19 +10,8 @@ module.exports = {
       pink : "#da919f",
       blue : "#9dc4bc",
       brown : "#d0b5ab",
-      white : "#fff"
-    },
-    container: {
-      center: true,
-    },
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px'
-    },
-    extend: {},
+      white : "#fff",
+    }
   },
-  plugins: [],
+  plugins:[],
 }
