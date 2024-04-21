60b872cab1376e52c9676c2b46f00a7cff1c8c31
Lao Oceane <oceane.lao1@gmail.com>
2024-04-21 18:30:30 +0200

    admin dashboard

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Appointment;
+use App\Entity\Review;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Coach Dev Perso');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Appointment', 'fa fa-calendar', Appointment::class);
+        yield MenuItem::linkToCrud('Review', 'fa fa-comments', Review::class);
+        yield MenuItem::linkToCrud('User', 'fa fa-user', User::class);
+    }
+}
