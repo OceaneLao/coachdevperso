6bf944f414d27a5bf819b52c5a34d2edebc1bc75
OceaneLao <oceane.lao1@gmail.com>
2024-04-22 16:56:13 +0200

    appointment page style loading

--- src/Controller/AppointmentController.php
+++ src/Controller/AppointmentController.php
@@ -1,7 +1,8 @@
 <?php
-
 namespace App\Controller;
 
+use App\Entity\Appointment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class AppointmentController extends AbstractController
 {
     #[Route('/appointment', name: 'app_appointment')]
-    public function index(): Response
+    public function displayAppointment(
+        EntityManagerInterface $entityManagerInterface
+        ): Response
     {
+        // Récupérer toutes les données dans le Repository
+        $appointmentRepository = $entityManagerInterface->getRepository(Appointment::class);
+        $appointments = $appointmentRepository->findAll();
+
         return $this->render('appointment/index.html.twig', [
-            'controller_name' => 'AppointmentController',
+            'appointments' => $appointments,
         ]);
     }
 }
