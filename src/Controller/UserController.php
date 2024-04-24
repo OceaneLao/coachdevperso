6c2a1917ebc4685d71f5aece29ba5bb22c8e22e8
OceaneLao <oceane.lao1@gmail.com>
2024-04-24 15:21:48 +0200

    display user space appointment

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Appointment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,11 +11,20 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index(
+        EntityManagerInterface $entityManagerInterface,
+    ): Response
     {
         $user = $this->getUser();
+        // dd($user);
+
+        $appointmentRepository = $entityManagerInterface->getRepository(Appointment::class);
+        $appointments = $appointmentRepository->findBy(['user'=>$user->getId()]);
+        // dd($appointments);
+
         return $this->render('user/index.html.twig', [
             'user'=> $user,
+            'appointments'=> $appointments,
         ]);
     }
 
